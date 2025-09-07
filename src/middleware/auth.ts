0e7c9301470e8f0966fbb6bc47c4825b10515567
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-09-07 19:11:39 +0300

    create types for auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,18 +1,19 @@
-const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
+import { Request, Response, NextFunction } from 'express';
+import { UserInterface } from '../types/User';
 
+interface AuthenticatedRequest extends Request {
+    user: UserInterface; 
+}
 
-
-const LoginAuth = async (req, res, next) => {
-    
+const loginAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-
         // making sure the headers has the authorization word
         const authHeader = req.headers['authorization'];
         // testing if we have the header or also we have have it with the word Bearer 
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ success: false, message: 'No token provided!' });
+            return res.status(401).json({ success: false, message: 'No token provided!' });
         }
 
         // extract the token from the headers
@@ -24,37 +25,29 @@ const LoginAuth = async (req, res, next) => {
 
         // Find the user and attach it back to the request and then Exclude password
         const user = await User.findById(decoded.id).select('-password');
-     
-        if(!user){
-            return res.status(401).json({success: false, message: 'Not authorized'});
+
+        if (!user) {
+            return res.status(401).json({ success: false, message: 'Not authorized' });
         }
         // sending the full user 
-        req.user = user ;
+        req.user = user;
         next();
 
     } catch (error) {
-        return res.status(401).json({ success : false , message :'User has nnot yet logged in with the error message : '});
+        return res.status(401).json({ success: false, message: 'User has not yet logged in with the error message : ' });
     }
 }
 
-
-
-
-const admin = async (req , res , next) =>{
-
- 
-        if(req.user && req.user.role === 'admin'){
+const admin = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    if (req.user && req.user.role === 'admin') {
         next();
-        } 
-        else {
-            res.status(403).json({ message: 'Access denied. Admin privileges required.' })
-        }
-        
-    
+    }
+    else {
+        res.status(403).json({ message: 'Access denied. Admin privileges required.' })
+    }
 }
+
 module.exports = {
-    LoginAuth,
+    loginAuth,
     admin
 }
-
-export {};
