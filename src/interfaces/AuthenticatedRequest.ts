c0d5b030b21785b9389ea4eb533d41164c0a4c82
Abdelrahman Sabaa <s90487@bht-berlin.de>
2025-09-13 19:02:58 +0200

    updating the requests to have the needed interfaces

--- src/interfaces/AuthenticatedRequest.ts
+++ src/interfaces/AuthenticatedRequest.ts
@@ -1,8 +1,13 @@
 import { UserInterface } from "./User";
+import {IProduct} from './Product'
+import { ICart } from "./CartInterface";
 // req.body, req.params, req.query
 interface AuthenticatedRequest extends Request {
     user: UserInterface;
+    product: IProduct;
     params: Record<string, any>;
+    query: any;
+
 }
 
 export type { AuthenticatedRequest };
