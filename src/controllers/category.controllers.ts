212711f3a2dd9e75096489e1ec27938c80a808d3
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-09-18 12:29:27 +0300

    enhance camelCase and add the enhanced object litrals

--- src/controllers/category.controllers.ts
+++ src/controllers/category.controllers.ts
@@ -1,102 +1,105 @@
 import { AuthenticatedRequest } from "../interfaces/AuthenticatedRequest";
 import { Request, Response } from "express";
-import { ICategory} from '../interfaces/Category'
+import { ICategory } from '../interfaces/Category'
 
 const Category = require('../models/category.model');
 
-const showcategories = async (req : AuthenticatedRequest , res :Response) => {
+const showcategories = async (req: AuthenticatedRequest, res: Response) => {
     try {
-    const showAllCategories =  await Category.find();
-    res.status(200).json({ 
-        success : true,
-        data : showAllCategories
-    })
+        const showAllCategories = await Category.find();
+        res.status(200).json({
+            success: true,
+            data: showAllCategories
+        })
 
     } catch (error) {
         console.log("Error from showcategories :", error.message);
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 }
-const createcategories = async (req : AuthenticatedRequest<ICategory> , res :Response) => {
-    const { name , slug , description } = req.body;
 
-    if(!name || !slug || !description){
-        return res.status(400).json({ message : 'All fields are required.!'})
+const createCategories = async (req: AuthenticatedRequest<ICategory>, res: Response) => {
+    const { name, slug, description } = req.body;
+
+    if (!name || !slug || !description) {
+        return res.status(400).json({ message: 'All fields are required.!' })
     }
     try {
-        
-        const searchedCate = await Category.findOne({ name : name})
-        if (searchedCate){
-            return res.status(400).json({ message : 'Category name has already been taken !'})
 
+        const searchedCate = await Category.findOne({ name });
+
+        if (searchedCate) {
+            return res.status(400).json({ message: 'Category name has already been taken !' })
         }
+
         const newCate = new Category({
-            name : name,
-            slug : slug,
-            description : description
+            name: name,
+            slug: slug,
+            description: description
         })
 
         await newCate.save();
-         res.status(201).json({ success : true,message : 'A new Category has been created' ,data : newCate
-    })
+        res.status(201).json({
+            success: true, message: 'A new Category has been created', data: newCate
+        })
 
     } catch (error) {
         console.log("Error from createcategories :", error.message);
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 }
-const updatecategorieById = async (req : AuthenticatedRequest<ICategory> , res :Response) => {
-        const categoryId = req.params.id;
-        const { name , slug , description } = req.body;
 
-    if(!name || !slug || !description){
-        return res.status(400).json({ message : 'All fields are required.!'})
+const updateCategorieById = async (req: AuthenticatedRequest<ICategory>, res: Response) => {
+    const categoryId = req.params.id;
+    const { name, slug, description } = req.body;
+
+    if (!name || !slug || !description) {
+        return res.status(400).json({ message: 'All fields are required.!' })
     }
 
     try {
 
-    const searchedCateByIdAndUpdate = await Category.findByIdAndUpdate(categoryId , req.body , { new: true});
-    if (!searchedCateByIdAndUpdate) {
-      return res.status(400).json({ success: false, message: "could not found the Category!" });
-    }
+        const searchedCateByIdAndUpdate = await Category.findByIdAndUpdate(categoryId, req.body, { new: true });
+        if (!searchedCateByIdAndUpdate) {
+            return res.status(400).json({ success: false, message: "could not found the Category!" });
+        }
 
-     res.status(200).json({
-      success: true,
-      message: "Category has been updated",
-      data: searchedCateByIdAndUpdate
-    })
+        res.status(200).json({
+            success: true,
+            message: "Category has been updated",
+            data: searchedCateByIdAndUpdate
+        })
 
     } catch (error) {
         console.log("Error from updatecategorieById :", error.message);
         res.status(500).json({ message: "Server Error", error: error.message });
-    
     }
-
 }
-const deletecategorieById = async (req : AuthenticatedRequest , res :Response) => {
+
+const deleteCategorieById = async (req: AuthenticatedRequest, res: Response) => {
     const categoryId = req.params.id;
 
     try {
         const searchedCateByIdAndDelete = await Category.findByIdAndDelete(categoryId);
-    if (!searchedCateByIdAndDelete) {
-      return res.status(404).json({ success: false, message: "No Category found with the given id" });
-    }
+        if (!searchedCateByIdAndDelete) {
+            return res.status(404).json({ success: false, message: "No Category found with the given id" });
+        }
 
-    res.status(200).json({ success: true, message: " the Category has been successfully deleted" });
+        res.status(200).json({ success: true, message: " the Category has been successfully deleted" });
 
     } catch (error) {
-        
-         console.log("Error from deletecategorieById :", error.message);
+
+        console.log("Error from deletecategorieById :", error.message);
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 }
-    
+
 
 module.exports = {
     showcategories,
-    createcategories,
-    updatecategorieById,
-    deletecategorieById
+    createCategories,
+    updateCategorieById,
+    deleteCategorieById
 }
 
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {LoginAuth , admin } = require('../middleware/auth');
-const {showcategories , createcategories , updatecategorieById ,deletecategorieById } = require('../controllers/category.controllers')
+const {showcategories , createCategories , updateCategorieById ,deleteCategorieById } = require('../controllers/category.controllers')
 
 
 router.get('/', showcategories);
-router.post('/', LoginAuth , admin ,createcategories)
-router.put('/:id', LoginAuth , admin ,updatecategorieById)
-router.delete('/:id', LoginAuth , admin , deletecategorieById)
+router.post('/', LoginAuth , admin ,createCategories)
+router.put('/:id', LoginAuth , admin ,updateCategorieById)
+router.delete('/:id', LoginAuth , admin , deleteCategorieById)
 
 
 module.exports = router;
